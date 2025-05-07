bf4ebed366d5633d6f05ebc0bcba9ecf4ba3bb3b
panawat <panawatsrimuang@gmail.com>
2025-05-07 10:03:57 +0700

    เพิ่ม

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';  
 import Modern from './Modern/Modern';
 import Navbar from './navbar/Navbar';
+import EcommerceDashboard from './Ecommerce/Ecommerce';
 
 function App() {
   return (
     <>
-      
       <Routes>
         <Route path="/modern" element={<Modern />} />
+        <Route path="/ecommerce" element={<EcommerceDashboard />} />
       </Routes>
     </>
   );
