fd4b87836eb0bc868e52832ac19016cc3bebca46
Kim Boolkinee <boolkinee9@gmail.com>
2025-05-07 13:39:41 +0700

    ส่ง

--- src/Ecommerce/Ecommerce.tsx
+++ src/Ecommerce/Ecommerce.tsx
@@ -4,12 +4,19 @@ const DashboardCard = () => {
     style={{ backgroundImage: "url('/welcome-bg.svg')",
       backgroundPosition: '200px 0px',
     }}>
-      <div className="flex items-center gap-4 mb-4">
+      <div className="flex items-center justify-start gap-4 mb-4 pl-6">
     <img
       src="/user-1.jpg" // รูปโปรไฟล์
       alt="Mathew"
       className="w-12 h-12 rounded-full object-cover shadow-md"
+      style={{ marginLeft: "20px", marginTop: "20px" }} //
     />
+     <div>      
+      <h2 className="text-lg font-bold text-black"
+  style={{ marginTop: "25px" }}>
+    Welcome back Mathew Anderson!
+  </h2>
+    </div>
     </div>
     </div>
   );
