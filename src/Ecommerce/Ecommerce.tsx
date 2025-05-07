98b7988024f4f249527a7fbb73e6381c06a8ed12
Kim Boolkinee <boolkinee9@gmail.com>
2025-05-07 13:09:56 +0700

    ส่ง

--- src/Ecommerce/Ecommerce.tsx
+++ src/Ecommerce/Ecommerce.tsx
@@ -1,12 +1,18 @@
-
-import React from 'react'
-
-const Ecommerce = () => {
+const DashboardCard = () => {
   return (
-    <div>
-      hello
+    <div className=" bg-[#EEF3FF] w-[752px] h-[196px] bg-[length:100%_100%] bg-left  rounded-md shadow-xl m-24"
+    style={{ backgroundImage: "url('/welcome-bg.svg')",
+      backgroundPosition: '200px 0px',
+    }}>
+      <div className="flex items-center gap-4 mb-4">
+    <img
+      src="/user-1.jpg" // รูปโปรไฟล์
+      alt="Mathew"
+      className="w-12 h-12 rounded-full object-cover shadow-md"
+    />
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Ecommerce
+export default DashboardCard;
